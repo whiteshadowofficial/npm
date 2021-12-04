b5a930c88e7ee060160f9b1d7936586c9ac4a81e
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-12-04 05:46:09 +0530

    Initial commit

--- plugins/var-apks.js
+++ plugins/var-apks.js
@@ -1,3 +1,10 @@
+/* # Exclusively from VIRUS FUCKER Project 
+# Do not use this fore any commercial thing
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved VIRUS FUCKER https://Wa.me/94779529221
+# Credit : usufusta-whatsappAsena
+*/
+
 const Asena = require('../events');
 const axios = require('axios');
 const {MessageType, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
