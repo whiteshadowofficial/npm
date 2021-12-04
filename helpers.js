3bd365a0d60dd53f1e68b3394a820d4227f25c7b
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-12-04 06:17:42 +0530

    Initial commit

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *Stefanie*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *Stefanie*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *Stefanie*:  ```" + msg + "```"
 }
 
 
